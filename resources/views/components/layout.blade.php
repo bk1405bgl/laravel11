1ab18df588990ef6ce51e2857e5f227fa2910cd0
Bilal Kuzey <bk1405bgl@gmail.com>
2024-03-31 09:01:23 -0700

    added dynamic routing

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,9 +17,9 @@
                     </div>
                     <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
-                        <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                        <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
-                        <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                        <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                        <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
+                        <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
                     </div>
                     </div>
                 </div>
@@ -67,7 +67,7 @@
             <div class="md:hidden" id="mobile-menu">
                 <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                     <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
-                    <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
+                    <a href="/jobs" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Jobs</a>
                     <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
                 </div>
                 <div class="border-t border-gray-700 pb-3 pt-4">
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,54 @@
 <?php
-
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '50k'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Developer',
+            'salary' => '40k'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Designer',
+            'salary' => '30k'
+        ]
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '50k'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Developer',
+            'salary' => '40k'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Designer',
+            'salary' => '30k'
+        ]
+        ];
+        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
